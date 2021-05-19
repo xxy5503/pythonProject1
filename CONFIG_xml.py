b55a47b59bd3dede6cfcb337f1e9aa602c7f8b6b
xuxueyue <xxy55.3@outlook.com>
2021-05-19 23:42:38 +0800

    test

---
+++ CONFIG_xml.py
@@ -0,0 +1,40 @@
+CONFIG0 = """
+    <config>
+      <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+        <interface>
+          <Loopback>
+            <name>102</name>
+            <description>loopback1-test</description>
+            <ip>
+              <address>
+                <primary>
+                  <address>1.1.1.10</address>
+                  <mask>255.255.255.255</mask>
+                </primary>
+              </address>
+            </ip>
+          </Loopback>
+        </interface>
+      </native>
+    </config>
+"""
+CONFIG1 = """
+    <config>
+      <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+        <interface>
+          <Loopback>
+            <name>202</name>
+            <description>loopback10-test</description>
+            <ip>
+              <address>
+                <primary>
+                  <address>1.1.1.100</address>
+                  <mask>255.255.255.255</mask>
+                </primary>
+              </address>
+            </ip>
+          </Loopback>
+        </interface>
+      </native>
+    </config>
+"""
---
+++ DeviceList.py
@@ -0,0 +1,3 @@
+devicelist = \
+    [{'name':'HomeGateWay','ip':'10.1.3.1','netconf_port':'830','username':'admin','password':'admin','params':'csr'}
+    ]
